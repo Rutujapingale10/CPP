58604d925481cee8424e91ad7a8b2793b665b802
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2023-02-17 15:13:32 +0530

    Create FactoryDesign.cpp

---
+++ C++ Design Pattern/FactoryDesign.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Vehicle{
+	public:
+		virtual void createVehicle()=0;
+		
+		
+};
+class bike:public Vehicle
+{
+	public:
+		void createVehicle()
+		{
+			
+		
+	        cout<<"creting bike"<<endl;
+        }
+};
+
+class car:public Vehicle{
+	public:
+		void createVehicle()
+		{
+			cout<<"Creating car"<<endl;
+		}
+};
+//factory Method
+class vehicleFactory{
+	public:
+	      static Vehicle* getVehicle(string vehicleType) //without creating obj we can access fun
+		{
+			
+			Vehicle* vehicle;
+				if(vehicleType=="car")
+				{
+					vehicle = new car();
+				}
+				else if(vehicleType=="bike")
+				{
+					vehicle=new bike();
+				}
+				else
+				{
+					cout<<"Wrong choice"<<endl;
+				}
+	
+				vehicle->createVehicle();
+		  	
+		}	
+};
+
+int main()
+{
+	string vehicleType;
+	cout<<"Enter type"<<endl;
+	cin>>vehicleType;
+	Vehicle* vehicle=vehicleFactory::getVehicle(vehicleType);
+}
+
+
+//without using factory
+/*
+int main()
+{
+	string vehicleType;
+	cout<<"Enter type"<<endl;
+	cin>>vehicleType;
+	Vehicle* vehicle;
+	if(vehicleType=="car")
+	{
+		vehicle = new car();
+	}
+	else if(vehicleType=="bike")
+	{
+		vehicle=new bike();
+	}
+	
+	vehicle->createVehicle();
+	
+	
+}*/
